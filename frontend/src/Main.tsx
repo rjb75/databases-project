b723f58f1ea5b1a250d3b8472f18680004fe56a1
Robert Brown <brown.rjb75@gmail.com>
2021-11-15 14:57:52 -0700

    feat: add prettier and eslint

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,26 +1,22 @@
 import * as React from 'react';
-import { App } from './App';
-import './styles/Main.scss'
+import {App} from './App';
+import './styles/Main.scss';
 
-export interface IMainProps
-{
-    app: App; // Reference to our App.ts class
+export interface IMainProps {
+  app: App; // Reference to our App.ts class
 }
 
-export class Main extends React.Component<IMainProps, {}>
-{
-    constructor(props: IMainProps)
-    {
-        super(props);
-    }
+export class Main extends React.Component<IMainProps> {
+  constructor(props: IMainProps) {
+    super(props);
+  }
 
-    public render(): JSX.Element
-    {
-        return (
-            <>
-                <h1>Databases Project</h1>
-                <p>Welcome to our codebase</p>
-            </>
-        );
-    }
+  public render(): JSX.Element {
+    return (
+      <>
+        <h1>Databases Project</h1>
+        <p>Welcome to our codebase</p>
+      </>
+    );
+  }
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,46 +1,55 @@
-const webpack = require("webpack");
-require("dotenv").config({ path: "../.env" });
+const webpack = require('webpack');
+require('dotenv').config({ path: '../.env' });
 
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.ENV,
   entry: {
-    app: ["./src/App.ts"],
-    vendor: ["react", "react-dom"],
+    app: ['./src/App.ts'],
+    vendor: ['react', 'react-dom'],
   },
-  watch: process.env.ENV == "development",
+  watch: process.env.ENV === 'development',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/[name].bundle.js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
-      }, {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
-      }
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }, {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
     ],
   },
-
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": JSON.stringify(process.env),
+      'process.env': JSON.stringify(process.env),
     }),
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
 };
