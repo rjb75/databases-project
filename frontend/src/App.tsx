96e2e238a26e52887c542db0a90dc95c0b59f556
Ahmed-I-Abdullah <ahmedi3aaa@hotmail.com>
2021-12-11 16:17:19 -0700

    feat: add basic forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import LogIn from './pages/Login/Login';
 import SampleSignout from './pages/SampleSignout/SampleSignout';
 import RoleSelection from './pages/RoleSelection/RoleSelection';
 import SignUp from './pages/SignUp/SignUp';
+import FormBuilder from './components/FormBuilder';
+import FormGenerator from './components/FormGenerator';
 
 import './styles/Main.scss';
 
@@ -14,7 +16,8 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<RoleSelection />} />
+          <Route path="/" element={<FormBuilder />} />
+          <Route path="/form" element={<FormGenerator formContent=''/>} />
           <Route path="/main" element={<SampleSignout />} />
         </Routes>
       </Router>
