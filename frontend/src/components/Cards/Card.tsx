a5a147659e0e7d6f94eaef11aa01f529d50d95fd
Robert Brown <brown.rjb75@gmail.com>
2021-12-13 02:25:38 -0700

    feat: card component updates

--- frontend/src/components/Cards/Card.tsx
+++ frontend/src/components/Cards/Card.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { CardProps } from './CardUtils';
+import './Card.scss'
 
 const Card: React.FC<CardProps> = props => {
     return (
-        <div className={`card card-small ${props.class || ''}`}>
+        <div className={`card card-${props.size || 'small'} ${props.class || ''}`} key={props.key || ''} >
             {props.children}
         </div>
     )
