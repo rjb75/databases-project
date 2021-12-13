a3ca58706097200157a2b3bb12c06731c78566b1
Robert Brown <brown.rjb75@gmail.com>
2021-12-13 13:48:50 -0700

    feat: implement api constants

--- frontend/src/pages/Sessions/SessionsPage.tsx
+++ frontend/src/pages/Sessions/SessionsPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axiosInstance from '../../axios';
 import StreamsListing from '../../components/Sessions/StreamsListing';
 import { formatStreams, Stream } from '../../components/Sessions/StreamUtils';
+import { ROOT_V1 } from '../../utils/APIConstants';
 
 const SessionsPage: React.FC = () => {
 
@@ -9,7 +10,7 @@ const SessionsPage: React.FC = () => {
 
     useEffect(() => {
         axiosInstance
-        .get('/api/v1/streams')
+        .get(`${ROOT_V1}/streams`)
         .then((res) => {
             setStreams(formatStreams(res.data.data))
         })
