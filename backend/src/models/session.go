52ed924452eb8bf6d1cb20915e9fcdd7bb875eb9
Robert Brown <brown.rjb75@gmail.com>
2021-12-16 16:55:39 -0700

    fix: update start time field

--- backend/src/models/session.go
+++ backend/src/models/session.go
@@ -3,7 +3,7 @@ package models
 type Session struct {
 	Session_number   string `json:"Session_number"`
 	Location         string `json:"Location"`
-	Start_time       int64  `json:"Start_time"`
+	Start_time       string `json:"Start_time"`
 	Duration_minutes int64  `json:"Duration_minutes"`
 	Title            string `json:"Title"`
 	Description      string `json:"Description"`
